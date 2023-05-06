fac89d8817352c49bf5002b3969aa9f8a46a9c4b
Miłosz Chudziński <milchu@st.amu.edu.pl>
2023-05-06 18:43:13 +0200

    Dodanie destroy,update,create

--- projects-service/app/controllers/api/projects_controller.rb
+++ projects-service/app/controllers/api/projects_controller.rb
@@ -11,5 +11,30 @@ module Api
       render json: @projects
     end
 
+    def create
+      @projects = Project.create(
+        name:params[:name],
+        city:params[:city],
+        client: params[:client]
+      )
+      render json: @projects
+    end
+
+    def update
+      @projects = Project.find(params[:id])
+      @projects.update(
+        name:params[:name],
+        city:params[:city],
+        client: params[:client]
+      )
+      render json: @projects
+    end
+
+    def destroy
+      @projects = Project.find(params[:id])
+      @projects.destroy
+      render json: @projects
+    end
+
   end
 end
--- projects-service/config/routes.rb
+++ projects-service/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-    resources :projects, only: [:index, :show]
+    resources :projects, only: [:index, :show, :create, :update, :destroy]
   end
 end
