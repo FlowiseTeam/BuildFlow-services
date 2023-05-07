7abb56fcb1344464c8f80bb45521328645434e37
jakcio <ciosekjakub@wp.pl>
2023-05-07 15:28:27 +0200

    fix _id v2

--- projects-service/app/models/project.rb
+++ projects-service/app/models/project.rb
@@ -18,22 +18,8 @@ class Project
   private
 
   def set_id
-    self._id = self.class.increment_counter(:projects)
+    last_project = self.class.order_by(_id: 'desc').first
+    self._id = last_project ? last_project._id + 1 : 1
   end
-
-  def self.increment_counter(name)
-    counter = Counter.find_or_create_by(name: name)
-    counter.inc(seq: 1)
-    counter.seq
   end
-end
-
-class Counter
-  include Mongoid::Document
-
-  field :name, type: String
-  field :seq, type: Integer, default: 0
-
-  index({ name: 1 }, { unique: true, name: "name_index" })
-end
 
