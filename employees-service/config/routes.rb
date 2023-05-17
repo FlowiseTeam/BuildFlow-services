44cbc999a7f530bed42d67e8c37fd7b95502e2c1
jakcio <ciosekjakub@wp.pl>
2023-05-17 22:53:27 +0200

    add correct routing /api/employee

--- employees-service/config/routes.rb
+++ employees-service/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :employees
+  namespace :api, defaults: { format: :json } do
+    resources :employees, only: [:index, :show, :create, :update, :destroy]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
