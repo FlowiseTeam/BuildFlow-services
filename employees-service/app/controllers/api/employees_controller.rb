9843f6e3e82b2546fb6999b270ed56bbc3845ae6
Miłosz Chudziński <milchu@st.amu.edu.pl>
2023-05-18 11:53:17 +0200

    Update employees CRUD

--- employees-service/app/controllers/api/employees_controller.rb
+++ employees-service/app/controllers/api/employees_controller.rb
@@ -1,55 +1,66 @@
 module Api
   class EmployeesController < ApplicationController
-    before_action :set_employee, only: %i[ show update destroy ]
 
     # GET /employees
     def index
       @employees = Employee.all
-
-      render json: @employees
+      @employees_count = Employee.count
+      render json: { employees: @employees, employees_count: @employees_count }
     end
 
     # GET /employees/1
     def show
       @employees = Employee.find(params[:id])
-      render json: @employees
+      render json: {employees: @employees}
     end
 
     # POST /employees
     def create
-      @employee = Employee.new(employee_params)
+      @employees = Employee.create(
+        first_name:params[:first_name],
+        last_name:params[:last_name],
+        role: params[:role],
+        assigned_project: params[:assigned_project],
+        status:params[:status]
+      )
 
-      if @employee.save
-        render json: @employee, status: :created, location: @employee
+      if @employees.save
+        render json: {
+          employees: @employees
+        }, status: :created
       else
-        render json: @employee.errors, status: :unprocessable_entity
+        render json: {
+          error: @employees.errors.full_messages.to_sentence
+        }, status: :unprocessable_entity
       end
     end
 
     # PATCH/PUT /employees/1
     def update
-      if @employee.update(employee_params)
-        render json: @employee
+      @employees = Employee.find(params[:id])
+      @employees.update(
+        first_name:params[:first_name],
+        last_name:params[:last_name],
+        role: params[:role],
+        assigned_project: params[:assigned_project],
+        status:params[:status]
+      )
+      if @employees.save
+        render json: {
+          employees: @employees
+        }, status: :ok
       else
-        render json: @employee.errors, status: :unprocessable_entity
+        render json: {
+          error: @employees.errors.full_messages.to_sentence
+        }, status: :unprocessable_entity
       end
     end
 
     # DELETE /employees/1
     def destroy
-      @employee.destroy
-    end
-
-    private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_employee
-      @employee = Employee.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def employee_params
-      params.require(:employee).permit(:id, :first_name, :last_name, :role, :qualifications, :assigned_project, :status)
+      @employees = Employee.find(params[:id])
+      @employees.destroy
+      head :no_content
     end
   end
-
 end
