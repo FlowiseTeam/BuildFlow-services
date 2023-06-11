f43b88a4265b7bbf78ffb3d4d1f0e5dbe2de81ef
jakcio <ciosekjakub@wp.pl>
2023-06-11 11:42:36 +0200

    add swagger

---
+++ projects-service/spec/requests/api/comments_spec.rb
@@ -0,0 +1,109 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/comments', type: :request do
+
+  path '/api/projects/{project_id}/comments' do
+    # You'll want to customize the parameter types...
+    parameter name: 'project_id', in: :path, type: :string, description: 'project_id'
+
+    get('list comments') do
+      response(200, 'successful') do
+        let(:project_id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    post('create comment') do
+      response(200, 'successful') do
+        let(:project_id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/projects/{project_id}/comments/{id}' do
+    # You'll want to customize the parameter types...
+    parameter name: 'project_id', in: :path, type: :string, description: 'project_id'
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('show comment') do
+      response(200, 'successful') do
+        let(:project_id) { '123' }
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    patch('update comment') do
+      response(200, 'successful') do
+        let(:project_id) { '123' }
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    put('update comment') do
+      response(200, 'successful') do
+        let(:project_id) { '123' }
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    delete('delete comment') do
+      response(200, 'successful') do
+        let(:project_id) { '123' }
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
---
+++ projects-service/spec/requests/api/employee_assignments_spec.rb
@@ -0,0 +1,49 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/employee_assignments', type: :request do
+
+  path '/api/projects/employee_assignments' do
+
+    get('show employee_assignment') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    post('create employee_assignment') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    delete('delete employee_assignment') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
